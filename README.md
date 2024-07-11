34dea6516c121b2788d4268678bda916e4d44f15
Rishabh Pandey <rishabhpandey8092@gmail.com>
2024-07-12 02:53:20 +0530

    Added initial server setup

---
+++ README.md
@@ -0,0 +1,8 @@
+# Welcome to Auth Service
+
+## Project setup
+- Clone the project on your local.
+- Execute `npm insall` on the same path as of your root directory of the clonned / downloaded project.
+- Create a `.env` file in the root diretory and add the following environment variables.
+    - `PORT = 3000`
+- Inside the `src/config` folder create a new file `config.json` and then add the following piece of json
---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { PORT } = require('./config/serverConfig');
+const ApiRoutes = require('./routes/index')
+
+function createAndStartServer(){
+    const app = express()
+    const router = express.Router();
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.use('/api', ApiRoutes);
+
+    app.listen(PORT, () => {
+        console.log(`Server is running at PORT ${PORT}`)
+    })
+}
+createAndStartServer()
