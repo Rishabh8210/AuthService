a5b4ba34b0885a31455cdd29a24772b5dc81464f
Rishabh Pandey <rishabhpandey8092@gmail.com>
2024-07-13 07:30:24 +0530

    Added email and password validation middlewared for the incomming request

---
+++ src/middlewares/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    AuthMiddlewareValidator: require('./authRequestValidator')
+}
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { UserController } = require('../../controllers/index');
+const { AuthMiddlewareValidator } = require('../../middlewares/index');
 const User = new UserController();
-router.post('/signup', User.createUser);
-router.post('/signin', User.signin);
+router.post('/signup', AuthMiddlewareValidator.validateUserAuth, User.createUser);
+router.post('/signin', AuthMiddlewareValidator.validateUserAuth,  User.signin);
 module.exports = router
